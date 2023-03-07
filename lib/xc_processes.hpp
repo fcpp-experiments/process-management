b2b79ef2b72bfe3b7f1e076e29a25e48bb7dcd3b
Gianluca Torta <giatorta@gmail.com>
2023-03-07 20:02:54 +0100

    defined first-cut lambda for spawn XC

--- lib/xc_processes.hpp
+++ lib/xc_processes.hpp
@@ -19,18 +19,20 @@
 namespace fcpp {
 
 //! @brief Handles a process, spawning instances of it for every key in the `key_set` and passing general arguments `xs` (overload with field<bool> status).
+//! @brief Handles a process, spawning instances of it for every key in the `key_set` and passing general arguments `xs` (overload with field<bool> status).
 template <typename node_t, typename G, typename S, typename... Ts, typename K = typename std::decay_t<S>::value_type, typename T = std::decay_t<std::result_of_t<G(K const&, Ts const&...)>>, typename R = std::decay_t<tuple_element_t<0,T>>, typename B = std::decay_t<tuple_element_t<1,T>>>
 std::enable_if_t<std::is_same<B,field<bool>>::value, std::unordered_map<K, R>>
-spawn(node_t& node, trace_t call_point, G&& process, S&& key_set, Ts const&... xs) {
+xcspawn(node_t& node, trace_t call_point, G&& process, S&& key_set, Ts const&... xs) {
     return spawn(node, call_point, [&](K const& k, auto const&... params){
         return nbr(node, call_point, field<bool>(false), [&](field<bool> n){
             bool b = false;
+            R ret;
             field<bool> fb = false;
-            if (any_hood(node, call_point, n) or key_set.count(k) > 0) {
-                fb = process(k, params...);
-                b = any_hood(node, call_point, fb) or other(fb);
+            if (coordination::any_hood(node, call_point, n) or key_set.count(k) > 0) {
+                tie(ret, fb) = process(k, params...);
+                b = coordination::any_hood(node, call_point, fb) or other(fb);
             }
-            return make_tuple(b, fb);
+            return make_tuple(make_tuple(ret, b), fb);
         });
     }, std::forward<S>(key_set), xs...);
 }
@@ -59,7 +61,8 @@ FUN common::option<message> get_message(ARGS, size_t devices) {
 
 
 //! @brief Result type of spawn calls dispatching messages.
-using message_log_type = std::unordered_map<message, times_t>;
+// TODO ****check
+using message_log_type = std::unordered_map<message, bool>;
 
 //! @brief Computes stats on message delivery and active processes.
 GEN(T) void proc_stats(ARGS, message_log_type const& nm, bool render, T) {
@@ -97,53 +100,61 @@ GEN(T) void proc_stats(ARGS, message_log_type const& nm, bool render, T) {
 //! @brief Export list for proc_stats.
 FUN_EXPORT proc_stats_t = export_list<message_log_type>;
 
-//! @brief Wave-like termination logic.
-template <typename node_t, template<class> class T>
-void termination_logic(ARGS, status& s, real_t v, message const& m, T<tags::wispp>) {
-    bool terminating = s == status::terminated_output;
-    bool terminated = nbr(CALL, terminating, [&](field<bool> nt){
-        return any_hood(CALL, nt) or terminating;
-    });
-    bool source = m.from == node.uid and old(CALL, true, false);
-    double ds = monotonic_distance(CALL, source, node.nbr_dist());
-    double dt = monotonic_distance(CALL, source, node.nbr_lag());
-    bool slow = ds < v * comm / period * (dt - period);
-    if (terminated or slow) {
-        if (s == status::terminated_output) s = status::border_output;
-        if (s == status::internal) s = status::border;
-    }
-}
-//! @brief Export list for termination_logic.
-FUN_EXPORT termination_logic_t = export_list<bool, monotonic_distance_t>;
-
-
 //! @brief Wrapper calling a spawn function with a given process and key set, while tracking the processes executed.
 GEN(T,G,S) void spawn_profiler(ARGS, T, G&& process, S&& key_set, real_t v, bool render) {
     // clear up stats data
     node.storage(tags::proc_data{}).clear();
     node.storage(tags::proc_data{}).push_back(color::hsva(0, 0, 0.3, 1));
     // dispatches messages
-    message_log_type r = spawn(node, call_point, [&](message const& m){
+    // TODO ***** RESUME correct call
+    message_log_type r = xcspawn(node, call_point, [&](message const& m){
         auto r = process(m);
-        real_t key = get<1>(r) == status::external ? 0.5 : 1;
+        // TODO **** adapt to field<bool>
+        //real_t key = get<1>(r) == status::external ? 0.5 : 1;
+        real_t key = 1;
         node.storage(tags::proc_data{}).push_back(color::hsva(m.data * 360, key, key));
         return r;
     }, std::forward<S>(key_set));
+    // message_log_type r = spawn(node, call_point, [&](message const& k){
+    //     return nbr(node, call_point, field<bool>(false), [&](field<bool> n){
+    //         bool b = false;
+    //         field<bool> fb = false;
+    //         if (coordination::any_hood(node, call_point, n) or key_set.count(k) > 0) {
+    //             tie(b,fb) = process(k);
+    //             b = coordination::any_hood(node, call_point, fb) or other(fb);
+    //         }
+    //         return make_tuple(b, fb);
+    //     });
+    // }, std::forward<S>(key_set));
+
+
     // compute stats
     proc_stats(CALL, r, render, T{});
 }
 //! @brief Export list for spawn_profiler.
-FUN_EXPORT spawn_profiler_t = export_list<spawn_t<message, status>, termination_logic_t, proc_stats_t>;
+FUN_EXPORT spawn_profiler_t = export_list<spawn_t<message, bool>, proc_stats_t>;
 
 
 //! @brief Makes test for spherical processes.
 GEN(T) void spherical_test(ARGS, common::option<message> const& m, T, bool render = false) { CODE
     spawn_profiler(CALL, tags::spherical<T>{}, [&](message const& m){
-        status s = node.uid == m.to ? status::terminated_output : status::internal;
-        return make_tuple(node.current_time(), s);
+        bool source = m.from == node.uid and old(CALL, true, false);
+        double ds = monotonic_distance(CALL, source, node.nbr_dist());
+        double dt = monotonic_distance(CALL, source, node.nbr_lag());
+
+        field<real_t> fdds = nbr(CALL, ds);
+        field<real_t> fddt = nbr(CALL, dt);
+
+        // TODO DUMMY to generate field of bool
+        field<bool> fdslow = (ds > dt);
+
+        //status s = node.uid == m.to ? status::terminated_output : status::internal;
+        // TODO ****check
+        //return make_tuple(node.current_time(), !fdslow);
+        return make_tuple(true, fdslow);
     }, m, node.storage(tags::infospeed{}), render);
 }
-FUN_EXPORT spherical_test_t = export_list<spawn_profiler_t>;
+FUN_EXPORT spherical_test_t = export_list<spawn_profiler_t, double, monotonic_distance_t>;
 
 //! @brief Main case study function.
 MAIN() {
